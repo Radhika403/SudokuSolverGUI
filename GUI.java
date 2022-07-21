ca151323d96b60adc74e605dfa8f65c7cc072dfe
Raghav Bansal <raghavbansal011@gmail.com>
2022-07-21 14:53:00 +0530

    Fixing the working and basic UI of sudoku GUI

--- GUI.java
+++ GUI.java
@@ -1,85 +1,158 @@
 import javax.swing.*;
 // import javax.swing.event.AncestorListener;
+import javax.swing.border.Border;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-// import Solver.java;
+import java.awt.event.KeyAdapter;
 // import java.util.*;
+// import Solver.java;
+import java.awt.event.KeyEvent;
 
 public class GUI extends Solver implements ActionListener{
-
-    
+	
+	private JFrame frame;
+	private JButton b1;
+	private JButton b2;
+	private JButton b3;
+	private JTextField[] fields;
 
     public GUI() {
-        JFrame frame = new JFrame();
+        frame = new JFrame();
         frame.setPreferredSize(new Dimension(800, 600));
 
         JPanel panel = new JPanel();
 
-        JTextField[] fields = new JTextField[81];
-        for (int x=0 ; x<81 ; x++){
-            fields[x] = new JTextField();
-        }
-
-
-        JButton b1 = new JButton("Solve");
-        JButton b2 = new JButton("Clear grid");
-        JButton b3 = new JButton(" Exit");
-
+        b1 = new JButton("Solve");
+        b1.addActionListener(e -> {
+	    	GUIToSudoku(fields);
+     	});
+        
+        b2 = new JButton("Clear grid");
+        b2.addActionListener(e -> {
+        	// clearing all JTextFields by setting them ""
+        	for (int i=0; i<81; i++) {
+        		fields[i].setText("");
+        		fields[i].setBackground(Color.LIGHT_GRAY);
+        	}
+     	});
+        
+        b3 = new JButton("Exit");
+        b3.addActionListener(e -> {
+        	// disposing the frame
+    	   frame.dispose();
+    	});
+        
+        fields = getClearedFields();
+        
         panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
         panel.setLayout(new GridLayout(10, 3)); //imported awt module for this
         for (int x=0 ; x<81 ; x++) {
            panel.add(fields[x]);
         }
-        panel.add(b1);panel.add(b2);panel.add(b3);
-
-
-        
+        panel.add(b1);
+        panel.add(b2);
+        panel.add(b3);
 
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Sudoku Solver");
         frame.pack();
         frame.setVisible(true);
-
-        // b1.addActionListener(this);
-        // public void actionPerformed(ActionEvent e) {
-        //     if(e.getSource() == b1){
-        //         GUIToSudoku(fields);
-        //     }
-        // }
     
     }
 
+    JTextField[] getClearedFields() {
+    	JTextField[] newFields = new JTextField[81];
+        for (int x = 0 ; x < 81 ; x++){
+        	newFields[x] = new JTextField();
+            JTextField f = newFields[x];
+            f.setHorizontalAlignment(JTextField.CENTER);
+            f.setBackground(Color.LIGHT_GRAY);
+            f.setEditable(true);
+            Border border = BorderFactory.createLineBorder(Color.BLACK, 1);
+            f.setBorder(border);
+            
+            f.addKeyListener(new KeyAdapter() {
+                public void keyPressed(KeyEvent ke) {
+                   char pressedKey = ke.getKeyChar();
+                   if (pressedKey == 8) {
+                	   f.setText("");
+                	   f.setBackground(Color.LIGHT_GRAY);
+                   } else if ((pressedKey >= '1' && pressedKey <= '9')) {
+                	   f.setText(""+pressedKey);
+                	   f.setEditable(false);
+                	   f.setBackground(Color.GRAY);
+                   } else {
+                	   f.setEditable(false);
+                   }
+                }
+             });
+            f.setEditable(true);
+        }
+        
+        return newFields;
+    }
+    
+    void populateFieldInteractive(JTextField f, int numberToUpdate, int initial) {
+    	f.setEditable(true);
+    	if (initial == 0) {
+    		f.setForeground(Color.BLUE);
+    	} else {
+    		f.setForeground(Color.BLACK);
+    	}
+		f.setText(""+numberToUpdate);
+		f.setEditable(false);
+    }
+    
+    void SudokuToGUI(int[][] solution, int[][] initialSudoku){
+    	for (int i=0; i<9; i++) {
+    		for (int j=0; j<9; j++) {
+    			populateFieldInteractive(fields[(i*9) + j], solution[i][j], initialSudoku[i][j]);
+    		}
+    	}
+    }
+    
+    void copySudoku(int[][] originalSudoku, int[][] copiedSudoku) {
+    	for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+            	copiedSudoku[i][j] = originalSudoku[i][j];
+            }
+        }
+    }
+    
     void GUIToSudoku(JTextField[] fields){
         SudokuClass inputPuzzle = new SudokuClass();
         for(int i=0; i<9; i++){
             for(int j=0; j<9; j++){
-                String val = fields[2 * i + j].getText();
-                if (val.equals("")){
+                String val = fields[9 * i + j].getText();
+                if (val.equals("")) {
                     inputPuzzle.sudoku[i][j] = 0;
-                }else{
-                    inputPuzzle.sudoku[i][j] = Integer.parseInt(fields[2 * i + j].getText());
+                } else {
+                    inputPuzzle.sudoku[i][j] = Integer.parseInt(val);
                 }
             }
         }
         
-        inputPuzzle.show();
-
-        solve(inputPuzzle, 0, 0);
-        // inputPuzzle.show();
-        // SudokuToGUI(inputPuzzle);
+        int[][] initialSudoku = new int[9][9];
+        copySudoku(inputPuzzle.sudoku, initialSudoku);
+        
+        int[][] solution = new int[9][9];
+        solveAndGetFirstSolution(inputPuzzle, 0, 0, solution);
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+                System.out.print(solution[i][j] + "  ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println("748743874398");
+        
+        SudokuToGUI(solution, initialSudoku);
     }
 
-    // @Override
-    // public void actionPerformed(ActionEvent e) {
-    //     if(e.getSource() == b1){
-    //         GUIToSudoku(fields);
-    //     }
-    // }
-
+     @Override
+     public void actionPerformed(ActionEvent e) {}
 
 }
-
-
--- Solver.java
+++ Solver.java
@@ -1,4 +1,3 @@
-
 public class Solver {
 
     public static boolean checkRow(SudokuClass test, int i, int j){
@@ -86,7 +85,7 @@ public class Solver {
         if (cell >= 1 && cell <= 9) return false;
         return true;
     }
-
+    
     public static void solve(SudokuClass test, int i, int j) {
         if (i == 8 && j == 9) {
             test.show();
@@ -110,34 +109,93 @@ public class Solver {
             solve(test, i, j+1);
         }
     }
+
+    public static void updateResult(int[][] result, int[][] solution) {
+    	for (int i=0; i<9; i++) {
+    		for (int j=0; j<9; j++) {
+    			solution[i][j] = result[i][j];
+    		}
+    	}
+    }
+    
+    public static void solveTemp(SudokuClass test, int i, int j, int[][] solution) {
+    	int[][] board = new int[9][9];
+    	
+    	solveAndGetFirstSolution(test, i, j, board);
+    	for (int k=0; k<9; k++) {
+    		for (int l=0; l<9; l++) {
+    			solution[k][l] = board[k][l];
+    		}
+    	}
+    }
+    
+    public static boolean solveAndGetFirstSolution(SudokuClass test, int i, int j, int[][] solution) { 	
+    	if (i == 8 && j == 9) {
+            updateResult(test.sudoku, solution);
+            return true;
+        }
+
+        if(j >= 9 && i < 8){
+        	if (solveAndGetFirstSolution(test, i+1,  0, solution)) {
+        		return true;
+        	}
+            return false;
+        }
+
+        if(isEmpty(test.sudoku[i][j])){
+            for(int val = 1; val <= 9; val++){
+                test.sudoku[i][j] = val;
+                if (checkRow(test, i, j) && checkColumn(test, i, j) && checkBox(test, i, j)) {
+                    if (solveAndGetFirstSolution(test, i, j+1, solution)) {
+                    	return true;
+                    }
+                }
+                test.sudoku[i][j] = 0;
+            }
+        } else {
+        	if (solveAndGetFirstSolution(test, i, j+1, solution)) {
+        		return true;
+        	}
+        }
+        
+        return false;
+    }
     
     
     public static void main(String[] args) {
         new GUI();
 
-        // int[][] board = {
-        //     { 8, 0, 0, 0, 0, 0, 0, 0, 0 },
-        //     { 0, 0, 3, 6, 0, 0, 0, 0, 0 },
-        //     { 0, 7, 0, 0, 9, 0, 2, 0, 0 },
-        //     { 0, 5, 0, 0, 0, 7, 0, 0, 0 },
-        //     { 0, 0, 0, 0, 4, 5, 7, 0, 0 },
-        //     { 0, 0, 0, 1, 0, 0, 0, 3, 0 },
-        //     { 0, 0, 1, 0, 0, 0, 0, 6, 8 },
-        //     { 0, 0, 8, 5, 0, 0, 0, 1, 0 },
-        //     { 0, 9, 0, 0, 0, 0, 4, 0, 0 } 
-        //   };
+         int[][] board = {
+             { 8, 0, 0, 0, 0, 0, 0, 0, 0 },
+             { 0, 0, 3, 6, 0, 0, 0, 0, 0 },
+             { 0, 7, 0, 0, 9, 0, 2, 0, 0 },
+             { 0, 5, 0, 0, 0, 7, 0, 0, 0 },
+             { 0, 0, 0, 0, 4, 5, 7, 0, 0 },
+             { 0, 0, 0, 1, 0, 0, 0, 3, 0 },
+             { 0, 0, 1, 0, 0, 0, 0, 6, 8 },
+             { 0, 0, 8, 5, 0, 0, 0, 1, 0 },
+             { 0, 9, 0, 0, 0, 0, 4, 0, 0 } 
+           };
 
-        // SudokuClass test = new SudokuClass(board);
-        // System.out.println("Original Sudoku");
-        // test.show();
-        // System.out.println();
+         SudokuClass test = new SudokuClass(board);
+         System.out.println("Original Sudoku");
+         test.show();
+         System.out.println();
 
-        // if (!isValid(test)){
-        //     System.out.println("Entered puzzle is invalid");
-        // } else {
-        //     System.out.println("Possible solutions ");
-        //     solve(test,0,0);
-        //     test.show();
-        // }
+         if (!isValid(test)){
+             System.out.println("Entered puzzle is invalid");
+         } else {
+             System.out.println("Solution");
+             int[][] sol = new int[9][9];
+             solveAndGetFirstSolution(test,0,0, sol);
+             for(int i=0; i<9; i++){
+                 for(int j=0; j<9; j++){
+                     System.out.print(sol[i][j] + "  ");
+                 }
+                 System.out.println();
+             }
+             System.out.println();
+             System.out.println("---------------------");
+         }
     }
 }
